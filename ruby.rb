55d6c63a25b0896500c782d8bbb3d934f65f26f7
rdavidreid <r.david.reid@gmail.com>
2016-04-07 20:14:44 -0700

    fizbuz

--- ruby.rb
+++ ruby.rb
@@ -2,3 +2,14 @@ def digitalRoot(arg)
   return arg if arg < 10
   return digitalRoot((arg % 10) + (arg / 10))
 end
+
+def fizbuz(arr)
+  ans = []
+  arr.each do |el|
+    if el % 3 == 0 && el % 5 == 0
+      ans += "fizzbuzz"
+    elsif
+    end
+  end
+
+end
